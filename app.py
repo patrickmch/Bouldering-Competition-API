700326faf09ab7f1949564ae31daffc1fdfd10dd
plc-patrick <patrick@privatelabelcampaigns.com>
2017-04-08 21:29:25 -0600

    eliminates redundant code in app.py

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-import pymongo
-import pprint
-from pymongo import MongoClient
 from flask import Flask, jsonify
-from bson.objectid import ObjectId
 from routes import api_routes
 
-client = MongoClient()
-db = client.test
-collection = db.test_collection
-posts = db.posts
 app = Flask(__name__)
 
 api_routes(app)
 
 @app.route('/')
 def index():
-    found_posts = posts.find_one()
-    all_posts = {}
-    for post in posts.find():
-        all_posts = post
-    return 'Index Page %s' % post
-    # return 'Index'
-    # 58e93bfee75ab54869e8967c
+    return 'Index'
 
 @app.route('/hello')
 def hello():
