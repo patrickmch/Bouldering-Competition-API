d258d9514cfc84b540f9b5cc2bd08b5091212c0c
plc-patrick <patrick@privatelabelcampaigns.com>
2017-04-12 10:53:19 -0600

    adds more in depth validation in try except block

--- crud.py
+++ crud.py
@@ -8,7 +8,7 @@ import pymongo
 import pprint
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
+import logging
 from participant import Participant
 
 client = MongoClient()
@@ -32,7 +32,9 @@ def create_participant():
     new_participant = request.get_json()
     try:
         new_id = posts.insert_one(new_participant).inserted_id
-    except:
-        return "Validation failed"
+    except pymongo.errors.WriteError as error:
+        #more detailed exceptions (eg. what fields were not filled out) are not possible with current Mongo validation
+        logging.error(error)
+        return "Failed to create a participant as one or more required fields were missing or incomplete"
     else:
         return str(new_id)
