d21ab675ae51494039f559704e263397e2047ec0
plc-patrick <patrick@privatelabelcampaigns.com>
2017-04-19 14:09:26 -0600

    debugs require_appkey function

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ from routes import api_routes
 from crud import *
 
 app = Flask(__name__)
-
-
-@app.route('/api/create_doc/<string:id>/<string:collection_name>', methods=['GET', 'POST'])
+generc_url_args = '<string:id>/<string:collection_name>/'
+generic_methods = ['GET', 'POST']
+@app.route('/api/create_doc/%s' % generc_url_args, methods= generic_methods)
 @require_appkey
 def create(collection_name, id):
     return create_doc(collection_name)
 
-@app.route('/api/create_user/', methods=['GET', 'POST'])
+@app.route('/api/create_user/', methods= generic_methods)
 def create_user():
     return create_doc('participants')
 
-@app.route('/api/find_doc/<string:collection_name>/<string:id>', methods=['GET', 'POST'])
+@app.route('/api/find_doc/%s' % generc_url_args, methods= generic_methods)
 @require_appkey
 def read(collection_name, id):
     return find_doc(collection_name, id)
 
-@app.route('/api/update_doc/<string:collection_name>', methods=['GET', 'POST'])
+@app.route('/api/update_doc/%s' % generc_url_args, methods= generic_methods)
 @require_appkey
 def update(collection_name, id):
     return update_doc(collection_name)
 
-@app.route('/api/delete_doc/<string:id>/<string:collection_name>', methods=['GET', 'POST'])
+@app.route('/api/delete_doc/%s' % generc_url_args, methods= generic_methods)
 @require_appkey
 def delete(collection_name, id):
-    return delete_doc(collection_name)
+    return delete_doc(collection_name, id)
--- crud.py
+++ crud.py
@@ -64,10 +64,14 @@ def delete_doc(collection_name, id):
 def require_appkey(view_function):
     @wraps(view_function)
     def decorated_function(*args, **kwargs):
-        app_key = ObjectId(kwargs.get("id"))
-        find_key = db.participants.find_one({"_id" : ObjectId(app_key)})
-        if find_key["_id"] == app_key:
-            return view_function(*args, **kwargs)
-        else:
-            abort(401)
+        try:
+            app_key = ObjectId(kwargs.get("id"))
+            find_key = db.participants.find_one({"_id" : ObjectId(app_key)})
+            if find_key["_id"] == app_key:
+                return view_function(*args, **kwargs)
+            else:
+                abort(401)
+        except:
+            return "invalid or no api key supplied"
+
     return decorated_function
