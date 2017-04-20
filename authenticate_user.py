88e6bc4b4984e912f0af1570460d55f9f1b127c8
plc-patrick <patrick@privatelabelcampaigns.com>
2017-04-20 13:06:26 -0600

    adds authorization function

--- authenticate_user.py
+++ authenticate_user.py
@@ -1,14 +1,34 @@
+from setup import *
 def require_appkey(view_function):
     @wraps(view_function)
     def decorated_function(*args, **kwargs):
-        try:
-            app_key = ObjectId(kwargs.get("id"))
-            find_key = db.participants.find_one({"_id" : ObjectId(app_key)})
-            if find_key["_id"] == app_key:
+        collection_name = kwargs.get("collection_name")
+        app_key = ObjectId(kwargs.get("id"))
+        user = db.participants.find_one({"_id" : ObjectId(app_key)})
+        if user["_id"] == app_key:
+            if user_is_auth(collection_name, user["role"]):
                 return view_function(*args, **kwargs)
             else:
-                abort(401)
-        except:
-            return "invalid or no api key supplied"
-
+                #user not authorized
+                abort(403)
+        else:
+            #user not authenticated
+            abort(401)
     return decorated_function
+
+def user_is_auth(collection_name, role):
+    authorization = {
+        'participants': {
+            'participant': True,
+            'admin': True
+        },
+        'competitions': {
+            'participant' : False,
+            'admin': True
+        },
+        'venue' : {
+            'participant' : False,
+            'admin' : True
+        }
+    }
+    return authorization[collection_name][role]
