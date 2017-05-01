84d85b2817b4092314a46ec77cc60837c8ef82b9
plc-patrick <patrick@privatelabelcampaigns.com>
2017-05-01 13:06:27 -0600

    creates new competitions_api class and finishes user_api

--- competitions_api.py
+++ competitions_api.py
@@ -4,32 +4,29 @@ class UserAPI(MethodView):
 
     def __init__(self, req):
         self.req = req
-        self.collection_name = "participants"
+        self.collection_name = "competitions"
 
-    # find a single user:
+    # find a single comp:
     def get(self):
         # TODO should we just return data here rather than trying to call crud?
         crud = Crud(self.request, collection_name = collection_name)
         crud.find_doc()
 
-    # create new user:
+    # create new comp:
     def post(self):
-        # if new_user check email to make sure it is not a duplicate account
-        query = self.collection.find({"email" : request["email"]})
-        if query.count() > 0:
-            return "The email you provided is already in use."
-        #create new datetime object for easier querying
-        self.request["birthday"] = datetime.strptime(request["birthday"], "%d/%m/%Y")
-        # encrypt password
-        self.request["password"] = pwd_encrypt.hash(self.request["password"])
+        self.collection == db.competitions:
+        request["comp_date"] = datetime.strptime(request["comp_date"], "%d/%m/%Y")
+        #add a venue id to the competitions to enforce a relationship between the two collections
+        request["venue_id"] = ObjectId(request["venue_id"])
         crud = Crud(self.request, collection_name = collection_name)
         crud.create_doc()
 
-    # delete a single user
+    # delete a single comp
     def delete(self):
         crud = Crud(self.request, collection_name = collection_name)
         crud.delete_doc()
 
+    #u
     def put(self, user_id):
         crud = Crud(self.request, collection_name = collection_name)
         crud.update_doc()
--- crud.py
+++ crud.py
@@ -4,19 +4,13 @@ class Crud:
         self.collection_name = kwargs.get("collection_name", "participants")
         self.collection = db[self.collection_name]
         self.request = kwargs.get("request")
-        self.user = kwargs.get("user", "new_user")
 
     def find_doc(self):
         info = self.collection.find_one(ObjectId(self._id))
         return str(info)
 
     def create_doc(self):
-
         request = self.request
-        # elif self.collection == db.competitions:
-        #     request["comp_date"] = datetime.strptime(request["comp_date"], "%d/%m/%Y")
-        #     #add a venue id to the competitions to enforce a relationship between the two collections
-        #     request["venue_id"] = ObjectId(request["venue_id"])
         try:
             new_id = self.collection.insert_one(request).inserted_id
         except pymongo.errors.WriteError as error:
@@ -48,6 +42,7 @@ class Crud:
         except:
             raise
         else:
+            #TODO do not reveal id
             if result.deleted_count < 1:
                 return "No records were deleted with id %s" % self.request["_id"]
             else:
