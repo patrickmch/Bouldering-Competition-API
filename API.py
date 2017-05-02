15950e8ee1d514abf4a84dcf8530647a133e3f38
plc-patrick <patrick@privatelabelcampaigns.com>
2017-05-02 17:57:46 -0600

    can_edit_request moved to API and create_crud_instance function created

--- API.py
+++ API.py
@@ -7,34 +7,49 @@ class API(MethodView):
     def __init__(self):
         self.req = request.get_json()
 
+    # check if the user is sending data they are authorized to edit
+    def can_edit_request(self):
+        if current_user.get_var('_id') == req_id:
+            # user is editing themself - always authorized
+            return True
+        elif current_user.get_var('_id') != req_id:
+            """pass the collection name and user role to determine if user is authorized to modify a collection
+            based on values stored in a dictionary"""
+            return authorization[collection_name][self.vars.get('role')]
+        else:
+            return False
+
     def handle_request(self):
         req = self.req
         return str(req)
     #TODO refactor here
-        # try:
-        #     req_id = req["_id"]
-        # except:
-        #     req_id = req[0]["_id"]
-        # if current_user.can_edit_request(req_id, collection_name, authorization):
-        #     return view_function(collection_name = collection_name, user = current_user, request = req)
-        # else:
-        #     #user not authenticated
-        #     abort(403, "You do not have permission to edit the requested resource.")
+        try:
+            req_id = req["_id"]
+        except:
+            req_id = req[0]["_id"]
+        if self.can_edit_request():
+            return view_function(collection_name = collection_name, user = current_user, request = req)
+        else:
+            #user not authenticated
+            abort(403, "You do not have permission to edit the requested resource.")
 
 
     def get(self):
-        crud = Crud(self.request, collection_name = collection_name)
+        create_crud_instance()
         crud.find_doc()
 
     def post(self):
         return self.handle_request()
-        crud = Crud(self.request, collection_name = collection_name)
+        create_crud_instance()
         crud.find_doc()
 
     def delete(self):
-        crud = Crud(self.request, collection_name = collection_name)
+        create_crud_instance()
         crud.delete_doc()
 
     def put(self, user_id):
-        crud = Crud(self.request, collection_name = collection_name)
+        create_crud_instance()
         crud.update_doc()
+
+    def create_crud_instance():
+        crud = Crud(self.request, collection_name = self.collection_name)
--- user.py
+++ user.py
@@ -7,18 +7,6 @@ class User:
     def get_var(self, var):
         return self.vars.get(var)
 
-    # check if the user is sending data they are authorized to edit
-    def can_edit_request(self, req_id, collection_name, authorization):
-        if self.vars.get("_id") == req_id:
-            # user is editing themself - always authorized
-            return True
-        elif self.vars.get('_id') != req_id:
-            """pass the collection name and user role to determine if user is authorized to modify a collection
-            based on values stored in a dictionary"""
-            return authorization[collection_name][self.vars.get('role')]
-        else:
-            return False
-
     # necessary for flask_login
     def is_active(self):
         return True
