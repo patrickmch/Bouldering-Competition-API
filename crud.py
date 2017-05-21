1fc64d041e4c36e6768556bc9ee789418e0bdc51
plc-patrick <patrick@privatelabelcampaigns.com>
2017-05-21 16:17:31 -0600

    all crud methods return json

--- crud.py
+++ crud.py
@@ -1,4 +1,5 @@
 from setup import *
+from response_handler import ErrorResponse
 class Crud:
     def __init__(self):
         self.collection = g.req.get_collection()
@@ -7,42 +8,46 @@ class Crud:
         # we should have already found db data in the process_request method when instantiate_req was called
         data = g.req.get_db_data()
         if data == None:
-            # if there was no data already, look again
-            data = self.collection.find_one(ObjectId(g.req.get_id()))
+            raise ErrorResponse(404)
         if g.req.get_collection_name() == 'participants':
             # remove the password and _id from the return set if this is a user
             data.pop('password', None)
             data.pop('_id', None)
-        return g._response.create_response(data)
+        response = {
+            'status_code' : 201,
+            'data' : data
+        }
+        return jsonify(response)
 
     def create_doc(self):
         try:
-            result = self.collection.insert_one(g.req.get_request()).inserted_id
-        except pymongo.errors.WriteError as error:
-            #more detailed exceptions (eg. what fields were not filled out) are not possible with current Mongo validation
-            logging.error(error)
-            return "Failed to insert the requested data in to the database as one or more fields was missing or incomplete"
-        else:
-            return "success %s" % str(result)
+            new_id = str(self.collection.insert_one(g.req.get_request()).inserted_id)
+        except pymongo.errors.WriteError:
+             raise ErrorResponse(400, 'Failed to insert the requested data in to the database because one or more fields was missing or incomplete')
+        response = {
+            'status_code' : 200,
+            '_id' : new_id
+        }
+        return jsonify(response)
+
 
     def update_doc(self):
         insert = g.req.get_request()
-        try:
-            result = self.collection.update_one({'_id' : ObjectId(g.req.get_id())}, {'$set' : insert}, False)
-        except Exception as e:
-            #TODO add logging function here...
-            result = e
-            # except bson.errors.InvalidId as error:
-            #     return "Invalid id"
-        return g._response.create_response(result)
+        result = self.collection.update_one({'_id' : ObjectId(g.req.get_id())}, {'$set' : insert}, False)
+        if result.matched_count < 1:
+            raise ErrorResponse(404)
+        response = {
+            'status_code' : 200
+        }
+        return jsonify(response)
 
 
 
     def delete_doc(self):
         result = self.collection.delete_one({"_id" : ObjectId(g.req.get_id())})
-        #TODO do not reveal id if it is a user id
-        return g._response.create_response(result)
-        # if result.deleted_count < 1:
-        #     return "No records were deleted with id %s" % g.req.get_id()
-        # else:
-        #     return "%s record with the id %s was deleted successfully" % (str(result.deleted_count), g.req.get_id())
+        if result.deleted_count < 1:
+            raise ErrorResponse(404)
+        response = {
+            'status_code' : 200
+        }
+        return jsonify(response)
--- response_handler.py
+++ response_handler.py
@@ -22,7 +22,7 @@ class ErrorResponse(Exception):
         err_messages = {
             400 : 'Some or all of your request was not valid and cannot be processed',
             401 : 'You are not authorized to access the requested resource, either because your credentials are not valid or you did not provide them',
-            403 : 'You are not do not have permission to access the requested resource',
+            403 : 'You do not have permission to access the requested resource',
             404 : 'The requested resource could not be found',
             500 : 'The server encountered an error'
         }
